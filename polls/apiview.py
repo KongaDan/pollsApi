7811e4fedb199b649450b51d845377cea0400283
Konga Dan <kongadan68@gmail.com>
2024-04-15 16:11:26 +0100

    creation des apiviews generics

--- polls/apiview.py
+++ polls/apiview.py
@@ -1,20 +1,25 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from .models import *
-from .serializers import PollSerializer
+from .serializers import *
+from rest_framework import generics
 
-class PollList(APIView):
-    def get(self, request):
-        polls=Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
-    
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll= get_object_or_404(Poll, pk=pk)
-        data= PollSerializer(poll).data
-        return Response(data)
-    
+#obtenir une liste d'entites ou en creer ,autorise get et post
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class=PollSerializer
 
+#recuperer les details d'une entite , autorise get et destroy
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset=Poll.objects.all()
+    serializer_class= PollSerializer
+    
+    
+class ChoiceList(generics.ListCreateAPIView):
+    #queryset specifie l'ensemble des requetes initiales
+    #serializer_class sera utiliser pour serialiser et deserialiser les donnees
+    queryset=Choice.objects.all()
+    serializer_class= ChoiceSerializer
+
+#Permet de creer des entites
+class CreateVote(generics.CreateAPIView):
+    serializer_class= VoteSerializer
     
--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
-from .apiview import PollDetail,PollList
+from .apiview import *
 
 urlpatterns = [
     path('polls/',PollList.as_view(), name="polls_list"),
     path('polls/<int:pk>',PollDetail.as_view(),name='poll_detail'),
+    path('choices/',ChoiceList.as_view(),name="choices_list"),
+    path('vote/',CreateVote.as_view(),name="create_vote"),
 ]
